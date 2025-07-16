6049ed9c09d5e41a060dec24b24baa9445d9fe8b
OSH <112930130+alpacoosh@users.noreply.github.com>
2025-07-16 16:08:36 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -237,7 +237,7 @@ if user is not None:
     """, unsafe_allow_html=True)
 
     if not st.session_state["agree_clicked"]:
-        c1, c2, c3 = st.columns([4.7, 3, 4.3])
+        c1, c2, c3 = st.columns([4.6, 3, 4.4])
         with c2:
             if st.button("이수 내역 확인 동의", key="agree_btn"):
                 st.session_state["agree_clicked"] = True
